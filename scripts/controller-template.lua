052e1512c0a4b8cd8d459041a9daa60a4cbaa31e
Vykstorm <victorruizgomezdev@gmail.com>
2019-02-10 01:41:56 +0100

    Removed bug

--- scripts/controller-template.lua
+++ scripts/controller-template.lua
@@ -6,8 +6,9 @@ if (event.type == 'interrupt' and event.iid == 'update') or
    ((event.type == 'on' or event.type == 'off') and event.pin == '%activation-pin%') then
 
    port.%output-port% = pin.%activation-pin% and mem.activations[%input%]
-else
-    mem.activations = {%activations%}
-end
+   interrupt(%interval%, 'update')
 
-interrupt(%interval%, 'update')
+elseif event.type == 'program' then
+    mem.activations = {%activations%}
+    interrupt(1, 'update')
+end
